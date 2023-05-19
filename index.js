877c2532cd9ddf362677e195fabdfc2e7de185c9
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-19 12:50:38 +0600

    added addToy post endpoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -9,13 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@project1.lispgny.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,43 +17,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const carToysCollection = client.db("carToys").collection("cars");
 
+    app.get("/",  (req, res) => {
+      res.send("helloooooo");
+    });
 
-   const carToysCollection = client.db('carToys').collection('cars');
-
-   app.get("/", (req, res) => {
-    res.send("helloooooo");
-  });
-
-
+    app.post("/addToy", async (req, res) => {
+      const toy = req.body;
+      console.log(toy)
+      const result = await carToysCollection.insertOne(toy);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
