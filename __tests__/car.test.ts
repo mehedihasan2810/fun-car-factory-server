a53500b43b71cf967a40cf632bf46d2a9b010590
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-04 17:52:33 +0600

    implemented test for car queries with jest

--- __tests__/car.test.ts
+++ __tests__/car.test.ts
@@ -1,17 +1,46 @@
 import { testServer } from ".";
 
+const GET_CARS = `#graphql 
+query GetCars {
+     getCars {
+        name
+        email
+        price
+        rating
+        url
+     } 
+    }`;
+
+const GET_CAR = `#graphql 
+query GetCar($id: String!) {
+     getCar(id: $id) {
+        name
+        email
+        price
+        rating
+        url
+     } 
+    }`;
+
 describe("Car Queries", () => {
-  it("checking jest", async () => {
-    const response = await testServer.executeOperation({
-      query: `#graphql 
-      query GetCars {
-           getCars {
-              name
-           } 
-          }`,
-      // variables: { name: "world" },
+  
+  it("fetches list of cars with field name, email, price, rating, url", async () => {
+    const getCarsRes = await testServer.executeOperation({
+      query: GET_CARS,
     });
 
-    expect(response).toMatchSnapshot();
+    expect((getCarsRes.body as any).singleResult.errors).toBeUndefined();
+    expect(getCarsRes).toMatchSnapshot();
   });
+
+  it("fetches single car with unique id arg", async () => {
+    const getCarRes = await testServer.executeOperation({
+      query: GET_CAR,
+      variables: { id: "64671a8fe81294b16783e44d" },
+    });
+
+    expect((getCarRes.body as any).singleResult.errors).toBeUndefined();
+    expect(getCarRes).toMatchSnapshot();
+  });
+
 });
